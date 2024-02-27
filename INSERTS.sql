8ece1e0dc816f0c8a6d099c09285a8e012ef39e6
AlexGM10 <alexgomo10@gmail.com>
2024-02-27 16:48:41 +0100

    [Update] Rubricatus

--- INSERTS.sql
+++ INSERTS.sql
@@ -29,62 +29,62 @@ VALUES
 
 -- select * from CAFE_CHOCOLATE;
 
-INSERT INTO CAFE_CHOCOLATE (ID, NOMBRE, PRECIO, STOCK) 
+INSERT INTO INFUSIONES (ID, NOMBRE, PRECIO, STOCK) 
 VALUES 
-    (1, 'CHOCOLATE_TAZA', 2.00, 10), 
-    (2, 'CAFE', 1.20, 10), 
-    (3, 'CAFE_LECHE', 1.50, 10), 
-    (4, 'CORTADO', 1.30, 10), 
-    (5, 'CAFE_AMERICANO', 1.45, 10), 
-    (6, 'CARAJILLO', 2.00, 10);
+    (1, 'TÉ NEGRO', 2.00, 10), 
+    (2, 'MANZANILLA', 1.20, 10), 
+    (3, 'MENTA POLEO PASION', 1.50, 10), 
+    (4, 'PASION PROHIBIDO', 1.30, 10), 
+    (5, 'TAJ MAHAL', 1.45, 10), 
+    (6, 'ABSENTA MINT', 2.00, 10),
+    (7, 'HERBAL RELAX', 3.00, 10);
 
--- select * from CAFE_CHOCOLATE;
+-- select * from INFUSIONES;
 
 
-INSERT INTO CAFE_CHOCOLATE (ID, NOMBRE, PRECIO, STOCK) 
+INSERT INTO AGUAS (ID, NOMBRE, PRECIO, STOCK) 
 VALUES 
-    (1, 'CHOCOLATE_TAZA', 2.00, 10), 
-    (2, 'CAFE', 1.20, 10), 
-    (3, 'CAFE_LECHE', 1.50, 10), 
-    (4, 'CORTADO', 1.30, 10), 
-    (5, 'CAFE_AMERICANO', 1.45, 10), 
-    (6, 'CARAJILLO', 2.00, 10);
-
--- select * from CAFE_CHOCOLATE;
+    (1, 'AGUA 0,5 L', 0.75, 10), 
+    (2, 'AGUA 1,5 L', 1.00, 10), 
+    (3, 'AGUA GAS VICHY', 1.50, 10);
 
 
-INSERT INTO CAFE_CHOCOLATE (ID, NOMBRE, PRECIO, STOCK) 
+-- select * from AGUAS;
+
+
+INSERT INTO ZUMOS (ID, NOMBRE, PRECIO, STOCK) 
 VALUES 
-    (1, 'CHOCOLATE_TAZA', 2.00, 10), 
-    (2, 'CAFE', 1.20, 10), 
-    (3, 'CAFE_LECHE', 1.50, 10), 
-    (4, 'CORTADO', 1.30, 10), 
-    (5, 'CAFE_AMERICANO', 1.45, 10), 
-    (6, 'CARAJILLO', 2.00, 10);
+    (1, 'ZUMO NARANJA', 1.25, 10), 
+    (2, 'ZUMO MELOCOTON', 1.20, 10), 
+    (3, 'ZUMO PIÑA', 1.30, 10), 
+    (4, 'ZUMO NARANJA NATURAL', 1.50, 10);
 
--- select * from CAFE_CHOCOLATE;
+-- select * from ZUMOS;
 
 
-INSERT INTO CAFE_CHOCOLATE (ID, NOMBRE, PRECIO, STOCK) 
+INSERT INTO REFRESCOS (ID, NOMBRE, PRECIO, STOCK) 
 VALUES 
-    (1, 'CHOCOLATE_TAZA', 2.00, 10), 
-    (2, 'CAFE', 1.20, 10), 
-    (3, 'CAFE_LECHE', 1.50, 10), 
-    (4, 'CORTADO', 1.30, 10), 
-    (5, 'CAFE_AMERICANO', 1.45, 10), 
-    (6, 'CARAJILLO', 2.00, 10);
+    (1, 'CACAOLAT', 2.00, 10), 
+    (2, 'COCA COLA', 1.20, 10), 
+    (3, 'COCA COLA CERO', 1.50, 10), 
+    (4, 'NESTEA', 1.30, 10), 
+    (5, 'AQUARIUS LIMON', 1.45, 10), 
+    (6, 'AQUARIUS NARANJA', 1.45, 10),
+    (7, 'FANTA LIMON', 1.45, 10),
+    (8, 'FANTA NARANJA', 1.45, 10),
+    (9, 'TONICA', 1.45, 10),
+    (10, 'BITTER', 2.00, 10);
 
--- select * from CAFE_CHOCOLATE;
+-- select * from REFRESCOS;
 
 
-INSERT INTO CAFE_CHOCOLATE (ID, NOMBRE, PRECIO, STOCK) 
+INSERT INTO CERVEZAS (ID, NOMBRE, PRECIO, STOCK) 
 VALUES 
-    (1, 'CHOCOLATE_TAZA', 2.00, 10), 
-    (2, 'CAFE', 1.20, 10), 
-    (3, 'CAFE_LECHE', 1.50, 10), 
-    (4, 'CORTADO', 1.30, 10), 
-    (5, 'CAFE_AMERICANO', 1.45, 10), 
-    (6, 'CARAJILLO', 2.00, 10);
+    (1, 'KINTO', 2.00, 10), 
+    (2, 'MEDIANAS', 1.20, 10), 
+    (3, 'MEDIANAS SIN ALCOHOL', 1.50, 10), 
+    (4, 'DAMM LEMON LATA', 1.30, 10), 
+    (5, 'CERVEZA LATA', 1.45, 10);
 
--- select * from CAFE_CHOCOLATE;
+-- select * from CERVEZAS;
 
